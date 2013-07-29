bd7f9d9c420d079618f483c60116801d591ccd0f
David Y. Ross <dyross@gmail.com>
2013-07-29 15:18:10 -0700

    add SyncApi

--- modules/executor/src/main/scala/api.scala
+++ modules/executor/src/main/scala/api.scala
@@ -6,6 +6,8 @@ import actors._
 
 import akka.actor._
 import akka.pattern.{ ask, pipe }
+import scala.concurrent.duration._
+
 import scala.concurrent._
 
 import org.apache.hadoop.fs._
@@ -39,5 +41,16 @@ object Api {
             val jarPath = fs.listStatus(path)(0).getPath.toString
             addProject(jarPath, name)
         }
+    }
+}
+
+object SyncApi {
+
+    def getProjects(): ProjectList = {
+        Await.result(Api.getProjects(), 1 minute)
+    }
+
+    def getProject(name: String): ProjectResult = {
+        Await.result(Api.getProject(name), 1 minute)
     }
 }
