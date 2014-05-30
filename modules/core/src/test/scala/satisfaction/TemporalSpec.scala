ce304c8c503be0753a713e6c115c348102eddd93
jeromebanks <jerome_banks@yahoo.com>
2014-05-30 16:18:26 -0700

    add time utilities

---
+++ modules/core/src/test/scala/satisfaction/TemporalSpec.scala
@@ -0,0 +1,44 @@
+package satisfaction
+
+
+import org.specs2.mutable._
+import org.junit.runner.RunWith
+import com.klout.satisfaction.Temporal
+import com.klout.satisfaction.Temporal._
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TemporalSpec extends Specification {
+
+  
+  "TemporalVariables" should {
+  
+    
+     "Produce hourly ranges" in {
+       Temporal.hours foreach println
+       
+       val hourSet = hours.toSet
+       
+       hourSet.size must_== 24
+     }
+     
+     "Produce time ranges" in {
+       
+        Temporal.dateRange( "20140401", "20140601") foreach println
+        
+        val days = dateRange( "20140401", "20140601").toList
+        println(" Size is " + days.size)
+        
+        days.take(1)(0)  must_==  "20140401"
+
+        days.takeRight(1)(0)  must_==  "20140531"
+          
+        days.size must_!=  0
+          
+     }
+     
+     
+  } 
+     
+     
+}
