2c1d22bd3ee0055613b1002184b39879cf78e30f
Yi Yin <yyin@yyin-laptop.tagged.com>
2014-06-09 12:46:27 -0700

    added fix for unschedule in willrogers - need to test

--- apps/willrogers/app/controllers/ScheduleTrackPage.scala
+++ apps/willrogers/app/controllers/ScheduleTrackPage.scala
@@ -65,9 +65,11 @@ object ScheduleTrackPage extends Controller {
    }
    
    def unscheduleOneTrack(trackName: String) = Action {
-     scheduler.unscheduleTrack(trackName) //pretty sure this is broken. Need a reference to its trackDescriptor!!!!
-      val scList = scheduler.getScheduledTracks.map(_._1).toSeq
-       val tdList = trackFactory.getAllTracks.diff(scList)
+     val desc = scheduler.getScheduledTracks.filter(_._1.trackName == trackName).last._1
+     scheduler.unscheduleTrack(desc) //YY this is gonna be broken due to versioning!
+     
+     val scList = scheduler.getScheduledTracks.map(_._1).toSeq
+     val tdList = trackFactory.getAllTracks.diff(scList)
      Ok(views.html.scheduletrack(tdList, scList)) 
    }
 }
