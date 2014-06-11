9fcbbd3a8d9ccf30074ff44e02484c2aaeb00e2a
Yi Yin <yyin@yyin-laptop.tagged.com>
2014-06-11 15:48:24 -0700

    3 functions completed for TrackHistory - need to finish others

--- modules/engine/src/main/scala/satisfaction/track/JDBCSlickTrackHistory.scala
+++ modules/engine/src/main/scala/satisfaction/track/JDBCSlickTrackHistory.scala
@@ -15,6 +15,7 @@ import java.sql.ResultSet
 import Q.interpolation
 import scala.slick.jdbc.meta.MTable
 import java.sql.Timestamp
+import java.util.Date
 
 
 
@@ -24,12 +25,11 @@ import java.sql.Timestamp
 
 
 class JDBCSlickTrackHistory extends TrackHistory{
-
- //encapsulate driver information
-	object H2DriverInfo {
-	  
-	  class TrackHistoryTable (tag: Tag) extends Table[(Int, String, String, String, String, String, String, Timestamp, Timestamp, String)](tag, "TrackHistoryTable") { // autoincrement is broken. I can't have id.? ~ for some reason...
-		  def id : Column[Int]= column[Int]("id", O.PrimaryKey, O.AutoInc)
+	
+ //case class TrackHistoryTable(val id: String, val trackName: String, val forUser:String, val version:String, val variant: String, val goalName: String, val witness: String, val startTime: Timestamp, val endTime: Timestamp, val state:String) {}
+	class TrackHistoryTable (tag: Tag) extends Table[(Int, String, String, String, String, String, String, Timestamp, Timestamp, String)](tag, "TrackHistoryTable") { // autoincrement is broken. I can't have id.? ~ for some reason...
+	//object trackEntry extends Table[(Int, String, String, String, String, String, String, Timestamp, Timestamp, String)]("TrackHistoryTable") {
+  			def id : Column[Int]= column[Int]("id", O.PrimaryKey, O.AutoInc)
 		  def trackName : Column[String] = column[String]("trackName")
 		  def forUser: Column[String] = column[String]("forUser")
 		  def version: Column[String] = column[String]("version")
@@ -43,8 +43,8 @@ class JDBCSlickTrackHistory extends TrackHistory{
 		  def * : ProvenShape[(Int, String, String, String, String, String, String, Timestamp, Timestamp, String)] = (id, trackName, forUser, version, variant, goalName, witness, startTime, endTime, state)
 		  
 		}
-	  
-	  
+ //encapsulate driver information
+	object H2DriverInfo {
 	  val JDBC_DRIVER : String =  "org.h2.Driver"
 	  val DB_URL : String = "jdbc:h2:file:data/sample" //change this to a file url, for persistence!
 	  val USER : String = "sa"
@@ -81,6 +81,14 @@ class JDBCSlickTrackHistory extends TrackHistory{
 	
 	override def completeRun( id : String, state : GoalState.State) : String = {
 	// update not a drop~~
+	  
+	  //might want to check the endDate timeStamp....
+	  H2DriverInfo.db withSession {
+	   implicit session =>
+	     val date : Date = new Date()
+	     
+	    Q.updateNA("UPDATE \"TrackHistoryTable\" SET endTime="+new Timestamp(date.getTime())+",state="+state.toString()+" WHERE id=" + id+";")
+	  }
 	  "Cheese"
 	}
 	
@@ -98,11 +106,31 @@ class JDBCSlickTrackHistory extends TrackHistory{
 	override def lookupGoalRun(  trackDesc : TrackDescriptor ,  
               goalName : String,
               witness : Witness ) : Seq[GoalRun] = {
+	 var returnList : GoalRun = null.asInstanceOf[GoalRun]
+	 /*
+	  H2DriverInfo.db withSession {
+	   implicit session =>
+	     val g = this.H2DriverInfo.table.filter(l => (l.)).list
+	   	
+	     val trackDesc :TrackDescriptor = TrackDescriptor(g(0)._2, g(0)._3, g(0)._4, Some(g(0)._5))
+	     returnGoal = GoalRun(trackDesc, g(0)._6, null, new DateTime(g(0)._8.toLocalDateTime()), null, GoalState.WaitingOnDependencies) // rewrite endtime and goalstate
+	   }
+	  Some(returnList)
 	  null
+	  * 
+	  */
 	}
 	
 	def lookupGoalRun( runID : String ) : Option[GoalRun] = { 
-	  null
+	  var returnGoal : GoalRun = null.asInstanceOf[GoalRun]
+	  H2DriverInfo.db withSession {
+	   implicit session =>
+	     val g = this.H2DriverInfo.table.filter(_.id === runID.toInt).list
+	   	
+	     val trackDesc :TrackDescriptor = TrackDescriptor(g(0)._2, g(0)._3, g(0)._4, Some(g(0)._5))
+	     returnGoal = GoalRun(trackDesc, g(0)._6, null, new DateTime(g(0)._8.toLocalDateTime()), null, GoalState.WaitingOnDependencies) // rewrite endtime and goalstate
+	   }
+	  Some(returnGoal)
 	}
 	
 	//dummy method
@@ -111,7 +139,7 @@ class JDBCSlickTrackHistory extends TrackHistory{
 	}
 	
 	def dummyStringToWitness(string : String ) : Witness = {
-	  null
+	   null
 	} 
 	
 	
