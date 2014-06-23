40b2a15f07ef99988efc2c60b6fa41df56908738
jeromebanks <jerome_banks@yahoo.com>
2014-06-23 13:13:27 -0700

    attempt to build project on jenkins

---
+++ .jenkins/job/suite.dsl.groovy
@@ -0,0 +1,32 @@
+@GrabResolver('https://artifactory.tagged.com/artifactory/libs-release-local/')
+@Grab('com.tagged.build:jenkins-dsl-common:0.1.20')
+
+import com.tagged.build.common.*
+
+def satisfaction = new Project(
+    jobFactory,
+    [
+        githubOwner: 'jbanks',
+        githubProject: 'satisfaction-limbo',
+        hipchatRoom: 'Cluster Corner',
+        email: 'jbanks@tagged.com',
+        jdk: 'jdk 7u51'
+    ]
+).basicJob {
+    description "Satisfaction Scheduler"
+    label "scala rpmbuild"
+    steps {
+        sbt('sbt',
+            'clean publish',
+            '-Dsbt.log.noformat=true -Dsbt.override.build.repos=true',
+            '-Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m')
+        sbt('sbt',
+            '\'project willrogers\' rpm:packageBin',
+            '-Dsbt.log.noformat=true -Dsbt.override.build.repos=true',
+            '-Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m')
+    }
+    }
+    triggers {
+        githubPush()
+    }
+}
